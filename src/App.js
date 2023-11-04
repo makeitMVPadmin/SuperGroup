5608eedaf789b996f8570b595e2a6f5effc9a9e2
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-11-04 12:19:31 -0400

    added chat page with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import ChatPage from "./pages/ChatPage/ChatPage"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/chat" element={<ChatPage/>}/>
     </Routes>
   );
 }
---
+++ src/components/ChatDetails/ChatDetails.jsx
@@ -0,0 +1,9 @@
+import "./ChatDetails.scss"
+
+const ChatDetails = () => {
+  return (
+    <div className="chatdetails">ChatDetails</div>
+  )
+}
+
+export default ChatDetails
