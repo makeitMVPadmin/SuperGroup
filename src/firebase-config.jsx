266482d96f958d6cc9a9cca356c7e07132fdb9e8
adelaguardiab@gmail.com <adelaguardiab@gmail.com>
2023-11-05 09:50:38 -0500

    small change

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, firebase } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
