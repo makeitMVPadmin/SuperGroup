31fb18e912fc877913b5d141c35056418acee229
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-11-12 23:08:49 -0500

    added sign in and chat functionality for testing ai

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -22,4 +22,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
