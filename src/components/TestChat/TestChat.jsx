d4eb39e1e891b6152b7c81d1a9240364c11421de
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-11-13 11:24:23 -0500

    updated test chat functionality to include more than one person in the chat

--- src/components/TestChat/TestChat.jsx
+++ src/components/TestChat/TestChat.jsx
@@ -9,31 +9,38 @@ import {
   query,
   where,
 } from "firebase/firestore"; //add a row in the db table, adds an entry (a document) into your collection
-import { unsubscribe } from "diagnostics_channel";
+import "./TestChat.scss";
 export const TestChat = (props) => {
   const { room } = props;
   const [newMessage, setNewMessage] = useState(""); //it is going to reflect what the user is typing in this input
-  const messagesRef = collection(db, "messages-test"); //reference to the messages collection
   const [messages, setMessages] = useState([]);
+
+  const messagesRef = collection(db, "messages-test"); //reference to the messages collection
   //so that firebase continuously automates our data, onsnapshot allows us to listen to those changes
   useEffect(() => {
     const queryMessages = query(
       messagesRef,
-      where("room", "===", room),
+      where("room", "==", room),
       orderBy("sentAt")
     );
     //snapshot requires a query that we want to listen to, so that we can recreate those changes, where is to only grab the ,essages from the room that we are in
     //grab messages woth snapshot variable
-    let messages = [];
-    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-      snapshot.forEach((doc) => {
-        //to get the data from each document
-        messages.push({ ...doc.data(), id: doc.id }); //object that contains all the previous data from the document and add an id to differentiate it form eachother, the id doesn't exist on doc.data, it exists in doc.id
-      }); //grab the data from the doc from doc.dataa, setting the new object of messages to be equal to whatever the data exists in the doc plus this new id
-      //update state to keep track of all the messages so that we can see the changes happening in real time, we dont have that state so far so we have to create that state by cretating
-      setMessages(messages);
+    console.log("gra");
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+      try {
+        let messages = [];
+        snapshot.forEach((doc) => {
+          //to get the data from each document
+          messages.push({ ...doc.data(), id: doc.id }); //object that contains all the previous data from the document and add an id to differentiate it form eachother, the id doesn't exist on doc.data, it exists in doc.id
+        }); //grab the data from the doc from doc.dataa, setting the new object of messages to be equal to whatever the data exists in the doc plus this new id
+        //update state to keep track of all the messages so that we can see the changes happening in real time, we dont have that state so far so we have to create that state by cretating
+        console.log(messages);
+        setMessages(messages);
+      } catch (error) {
+        console.error("Error fetching messages: ", error);
+      }
     });
-    return () => unsubscribe(); //return and clean up the useeffect for performance
+    return () => unsuscribe(); //return and clean up the useeffect for performance
   }, []);
   const handleSubmit = async (e) => {
     //prevent page from refreshing
@@ -46,16 +53,19 @@ export const TestChat = (props) => {
       sentBy: auth.currentUser.displayName,
       room: room,
     }); //collection, what message object you want to add
+    console.log(auth.currentUser);
     setNewMessage("");
   };
 
   return (
     <div className="chat">
-      <div className="chat__header">Welcome to: {room.toUpperCase()}</div>
+      <div className="chat__header">
+        <h1>Welcome to: {room.toUpperCase()}</h1>
+      </div>
       <div className="chat__messages">
         {messages.map((message) => (
           <div className="chat__message" key={message.id}>
-            <span className="chat__user">{message.sentBy}</span>
+            <span className="chat__user">{message.sentBy}:</span>
             {message.messageText}
           </div>
         ))}
