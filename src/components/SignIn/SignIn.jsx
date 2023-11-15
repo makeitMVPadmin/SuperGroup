7736dbb3df16728b8400266cd5e6dce2f66ee28a
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-11-15 15:53:04 -0500

    added user redundancy prevention to sign-in component

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import { getAuth, signInWithCustomToken } from "firebase/auth";
 import { useUser } from "@clerk/clerk-react";
-import { addDoc, collection } from "firebase/firestore";
+import { doc, getDoc, setDoc, addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebase-config"
 
 
@@ -18,6 +18,8 @@ const Authentication = () => {
   const uid = user.id;
   const photoUrl = user.imageUrl;
   console.log(fullName, email, uid , photoUrl)
+
+  const userRef = doc(db, "users", uid);
   
   
   useEffect(() => {
@@ -29,12 +31,26 @@ const Authentication = () => {
         const userCredentials = await signInWithCustomToken(auth, token);
         console.log(userCredentials);
         console.log("Authenticated user:", userCredentials.user);
-        await addDoc(collection(db, "users"), {
-          displayName: fullName,
-          email: email,
-          photoUrl: photoUrl,
-          uid: uid
-      });
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+          // A document with the same UID already exists, handle it accordingly (e.g., update or show an error).
+          console.log("User already exists:", userDoc.data());
+        } else {
+          // No document with the same UID exists, so you can add the new user data.
+          await setDoc(userRef, {
+            displayName: fullName,
+            email: email,
+            photoUrl: photoUrl,
+            uid: uid
+          });
+          console.log("User added:", uid);
+        }
+      //   await addDoc(collection(db, "users"), {
+      //     displayName: fullName,
+      //     email: email,
+      //     photoUrl: photoUrl,
+      //     uid: uid
+      // });
         await addDoc(collection(db, "userChats"),{ });
       } catch (error) {
         console.error("Error signing in with Clerk token:", error);
